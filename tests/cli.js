4d5fa25ae663e83221a5d7a23a3d2d01391021df
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-03-29 15:35:42 +0200

    Update test suite

--- tests/cli.js
+++ tests/cli.js
@@ -1,7 +1,6 @@
 var fs = require('fs')
 var assert = require('assert')
 var exec = require('child_process').execSync
-var transync = require('../src')
 var yaml = require('js-yaml')
 var path = require('path')
 
@@ -17,7 +16,7 @@ function readTestFile (name) {
   return yaml.safeLoad(fs.readFileSync('tests/' + name, 'utf8'), { json: true })
 }
 
-describe('The Node API', function () {
+describe('The CLI API', function () {
   var dummyData = {
     firstName: 'Hugo',
     lastName: 'Giraudel',
@@ -34,7 +33,7 @@ describe('The Node API', function () {
 
   it('should create the --to file if doesn’t exist', function () {
     createTestFile('en.json', dummyData)
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     assert.doesNotThrow(function () {
       fs.readFileSync('tests/de.json', 'utf8')
     }, Error)
@@ -42,21 +41,21 @@ describe('The Node API', function () {
 
   it('should have the same keys in synced YAML file', function () {
     createTestFile('en.yml', dummyData)
-    transync({ from: 'tests/en.yml', to: 'tests/de.yml' })
+    exec('./bin/transync --from tests/en.yml --to tests/de.yml')
     assert.deepEqual(readTestFile('en.yml'), readTestFile('de.yml'))
   })
 
   it('should have the same keys in synced JSON file', function () {
     createTestFile('en.json', dummyData)
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     assert.deepEqual(readTestFile('en.json'), readTestFile('de.json'))
   })
 
   it('should not override existing keys', function () {
     createTestFile('en.json', dummyData)
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     createTestFile('de.json', Object.assign({}, readTestFile('de.json'), { age: 42 }))
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     assert.equal(readTestFile('de.json').age, 42)
   })
 
@@ -64,11 +63,11 @@ describe('The Node API', function () {
     createTestFile('en.json', Object.assign({}, dummyData, {
       languages: { english: 'fluent', french: 'fluent' }
     }))
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     var de = readTestFile('de.json')
     de.languages.french = 'native'
     createTestFile('de.json', de)
-    transync({ from: 'tests/en.json', to: 'tests/de.json' })
+    exec('./bin/transync --from tests/en.json --to tests/de.json')
     assert.equal(readTestFile('de.json').languages.french, 'native')
   })
 })
